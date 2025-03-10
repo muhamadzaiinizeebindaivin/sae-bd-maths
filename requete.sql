39837f7af735925900d78c59040aa5672f5e64bb
NiksanNagarajah <niksan.nagarajah@etu.univ-orleans.fr>
2025-03-10 16:20:38 +0100

    Finalisation de la requete 4

--- requete.sql
+++ requete.sql
@@ -5,5 +5,10 @@
 -- 3. Compter le nombre de pièces composées existantes.
 
 -- 4. Calculer le coût total de chaque pièce composée.
+SELECT p.idPiece, p.nom, p.description, p.prixUnitaire, SUM(c.quantite * pc.prixUnitaire) AS coutTotal
+FROM PIECE p 
+JOIN COMPOSITION c ON p.idPiece = c.idComposee
+JOIN PIECE pc ON c.idComposant = pc.idPiece
+GROUP BY p.idPiece, p.nom, p.description, p.prixUnitaire; 
 
 -- 5. Trouver toutes les pièces (directes et indirectes) entrant dans la fabrication d’une pièce composée.
