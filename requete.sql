ec021f64a502a12f17aed506c5e6dd23876bbc9e
Lima Romain <o22301253@or-iut-i002-f16.iut45.univ-orleans.fr>
2025-03-10 16:23:22 +0100

    réalisation des requêtes 2 et 3

--- requete.sql
+++ requete.sql
@@ -2,7 +2,15 @@
 
 -- 2. Lister les pièces composées triées par le nombre de composants qu’elles contiennent.
 
+
+select idPiece, nom, description,prixUnitaire, estComposee, count(idComposant) as nbcomposant from PIECE join COMPOSITION on idPiece = idComposee group by idPiece;
+
+
+
 -- 3. Compter le nombre de pièces composées existantes.
+select count(distinct idPiece) as nombre_pieces_composees
+from PIECE
+where estComposee = 1;
 
 -- 4. Calculer le coût total de chaque pièce composée.
 
