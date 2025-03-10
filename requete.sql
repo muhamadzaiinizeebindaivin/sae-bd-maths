59312481aca1846188acdb6e49f23989733e98e6
muhamadzaiinizeebindaivin <muhamad.bin-daivin@etu.univ-orleans.fr>
2025-03-10 15:48:31 +0100

    Création de la liste des requêtes

---
+++ requete.sql
@@ -0,0 +1,9 @@
+-- 1. Lister les composants directs d’une pièce composée spécifique avec leur quantité.
+
+-- 2. Lister les pièces composées triées par le nombre de composants qu’elles contiennent.
+
+-- 3. Compter le nombre de pièces composées existantes.
+
+-- 4. Calculer le coût total de chaque pièce composée.
+
+-- 5. Trouver toutes les pièces (directes et indirectes) entrant dans la fabrication d’une pièce composée.
